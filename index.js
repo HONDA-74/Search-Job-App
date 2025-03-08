84defe417b03f4147270d1a0ace6f0b2f5f14156
HONDA-74 <mohanadtarek742203@gmail.com>
2025-03-08 21:47:30 +0200

    build code structure

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express'
 import bootstrap from './src/app.controller.js'
 import './src/utils/otp/deleteExpiredOtp.js'
 import { initSocket } from './src/socketIO/index.js'
-
+//test
 const app = express()
 const port = process.env.PORT || 3000
 await bootstrap(app,express)
